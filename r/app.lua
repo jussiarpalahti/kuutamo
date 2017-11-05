c8545de45db74b5c8edf26ff0b84b0ebe7827ab2
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-11-05 11:46:27 +0200

    Reading while into matrix works

--- r/app.lua
+++ r/app.lua
@@ -5,18 +5,55 @@ package.path = package.path .. ";./pl/?.lua"
 package.path = package.path .. ";./lua_modules/share/lua/5.1/?.lua"
 
 local cjson = require "cjson"
-
 local pretty = require 'pl.pretty'
+local stringx = require 'pl.stringx'
+local seq = require( 'pl.seq' )
+local tablex = require( 'tablex' )
+
+function read_px_file( ... )
+	-- read file, split into header and data part
+end
+
+function parse_meta( ... )
+	-- meta to table
+end
+
+function parse_data( ... )
+	-- data to matrix
+end
+
+function select_data( ... )
+	-- row and col index lists to matrix subset
+end
 
 
-function render()
+function get_meta()
 
-	t = {key1 = 'value1', key2 = false, key3 = 'jee'}
+	-- respond to request for PX file description
+
+	d = io.open('data/d.txt'):read('*a')
 
 	ngx.header.content_type = "application/json"
 	
-	ngx.say(cjson.encode(t))
+	res = {}
+	l = stringx.split(d, '\n')
+
+	for i = 1, #l do
+		if l[i] ~= "" then
+			res[i] = stringx.split(l[i])
+		end
+	end
+
+
+	ngx.say(cjson.encode(res))
 	
-	-- require('resty.repl').start()
 
 end
+
+
+function get_data( ... )
+	-- respond to requests for matrix subset
+end
+
+
+get_meta();
